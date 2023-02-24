e28044aa9f843111d6d4ac772816738d8194c57c
Kerry <kerryhokanson@gmail.com>
2023-02-24 17:26:13 -0600

    this may be it

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Project from './pages/Projects';
